46d2d72ebdc4bc992b61208e27c283e3f0144d7f
Jose Ricaurte <jricaurte@caci.com>
2018-03-01 02:07:59 -0500

    Add reload to dev mode and set colors for application output

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
-require('roosevelt')().startServer()
+/**
+ * Entry point for CADRE
+ * @module app
+ */
+
+// set color themes for application cli output
+let colors = require('colors');
+colors.setTheme({
+  warn: 'yellow',
+  error: 'red'
+});
+
+// warn if dependencies are out of date
+let output = require('check-dependencies').sync();
+if (!output.depsWereOk) {
+  console.warn(('📦  Dependencies are out of date! You may need to run npm i').warn.bold);
+}
+
+// init server
+let app;
+let reload = require('reload');
+let WebSocket = require('ws');
+let WebSocketServer = require('ws').Server;
+
+app = require('roosevelt')({
+    onServerInit: function(application) {
+      if (process.env.NODE_ENV === 'development') {
+        reloadReturned = reload(application, { webSocketServerWaitStart: true }); // On default port 9856
+      }
+  
+      let server = application.httpServer;
+      wss = new WebSocketServer({ server: server });
+  
+      wss.on('connection', function connection(ws) {
+        ws.on('message', function incoming(message) {
+          wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+              client.send(message);
+            }
+          });
+        });
+      });
+  
+      wss.broadcast = function(data) {
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+          }
+        });
+      };
+
+    },
+  
+    onServerStart: function(application) {
+      if (process.env.NODE_ENV === 'development') {
+        reloadReturned.startWebSocketServer();
+      }
+    }
+  });
+
+app.startServer();
--- mvc/models/global.js
+++ mvc/models/global.js
@@ -1,13 +1,27 @@
+/**
+ * Module is the global model for all CADRE responses. Returns application title, page title,
+ * current year, header data, and Node environment variables
+ * @module models/global
+ * @param {Object} req - Express req object
+ * @param {Object} res - Express res object
+ * @return {Global} - Global model object
+ */
 // sample static global base model
 let model = {
   content: {
-    appTitle: 'cadre',
+    appTitle: 'CADRE',
     pageTitle: '{content.appTitle}'
   }
-}
+};
 
-// extend global model provide additional useful vars at runtime and export it
+// extend global model to provide additional useful vars at runtime and export it
 module.exports = function (req, res) {
+  var scripts = ['/js/main.js'];
+
+  if (process.env.NODE_ENV === 'development') {
+    scripts.push('/reload/reload.js')
+  }
+
   return {
 
     // always static
@@ -16,6 +30,7 @@ module.exports = function (req, res) {
     // recalculated each require
     currentYear: new Date().getFullYear(),
     mainDomain: req.headers['x-forwarded-host'] || req.headers.host,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    scripts: scripts
   }
 }
