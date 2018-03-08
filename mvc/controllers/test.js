5fe24fb260ff8cc7cb181e4184032a04a6eefbc5
Jose Ricaurte <jricaurte@caci.com>
2018-03-07 22:10:57 -0500

    Add test controller

---
+++ mvc/controllers/test.js
@@ -0,0 +1,18 @@
+/**
+ * Module for Test view
+ * @module controllers/test
+ * @param {Object} app - Express App instance
+ */
+module.exports = function (app) {
+    /**
+     * GET Renders test page
+     * @event GET /test
+     * @param {Object} req - Express req object
+     * @param {Object} res - Express res object
+     */
+    app.route('/test').get((req, res) => {
+      let model = require('models/global')(req, res)
+      model.content.pageTitle = '{content.appTitle} - Info'
+      res.render('test', model)
+    })
+  }
