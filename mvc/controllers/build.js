ba715112322e9a2a691b1a2007f103c5ecbc3875
Jose Ricaurte <jricaurte@caci.com>
2018-03-07 22:08:48 -0500

    Add build controller

---
+++ mvc/controllers/build.js
@@ -0,0 +1,18 @@
+/**
+ * Module for Build view
+ * @module controllers/build
+ * @param {Object} app - Express App instance
+ */
+module.exports = function (app) {
+    /**
+     * GET Renders build page
+     * @event GET /build
+     * @param {Object} req - Express req object
+     * @param {Object} res - Express res object
+     */
+    app.route('/build').get((req, res) => {
+      let model = require('models/global')(req, res)
+      model.content.pageTitle = '{content.appTitle} - Build'
+      res.render('build', model)
+    })
+  }
